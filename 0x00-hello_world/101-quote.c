aa9b16b4639f22612f460016c47055a4f7e4edc2
Emmanuella208 <emmauellambaocha@gmail.com>
2022-10-18 00:49:56 -0700

    Advanced task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * main - points exactly and that peice of art is useful
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	write(2, " and that peice of art is useful\" - Dora korper, 2015-10-19\n", 59);
+	return (0);
+}
