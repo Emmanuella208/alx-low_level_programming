dba07d8496a3e56a8dd4dbab3bbbf08b18cd4229
Emmanuella208 <emmauellambaocha@gmail.com>
2022-10-18 01:30:55 -0700

    Advanced task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - points exactly and that peice of art is useful
- *
- * Return: Always 1 (success)
+ * main - Entry point
+ * Return: Always 1
  */
 
 int main(void)
 {
-	write(2, "and that peice of art is useful\" - Dora korper, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
